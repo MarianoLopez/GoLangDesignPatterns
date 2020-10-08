14a534f2eb3eb12a36ffc6e805e69c96a5d6b142
mariano lopez <m_villa@hotmail.com>
2020-10-08 09:57:00 -0300

    Builder & Factories

---
+++ internal/pattern/creational/factories/01_FactoryFunction/FactoryFunction.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age int
+}
+
+func NewPerson(name string, age int) *Person {
+	return &Person{name, age}
+}
+
+func main() {
+	// initialize directly
+	p := Person{"John", 22}
+	fmt.Println(p)
+
+	// use a constructor
+	p2 := NewPerson("Jane", 21)
+	p2.Age = 30
+	fmt.Println(p2)
+}

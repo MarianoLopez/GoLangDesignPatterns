6ae512374d7ef9e5ea5dcc2bad44a4254ee9955e
mariano lopez <m_villa@hotmail.com>
2020-10-14 08:41:11 -0300

    deep copy

--- internal/pattern/creational/Prototype/DeepCopy/DeepCopy.go
+++ internal/pattern/creational/Prototype/DeepCopy/DeepCopy.go
@@ -1,1 +1,61 @@
 package main
+
+import "fmt"
+
+type Address struct {
+	StreetAddress, City, Country string
+}
+
+type Person struct {
+	Name string
+	Address *Address
+	Friends []string
+}
+
+func (p *Person) DeepCopy() *Person {
+	q := *p
+	q.Address = p.Address.DeepCopy()
+	copy(q.Friends, p.Friends)
+	return &q
+}
+
+func (a *Address) DeepCopy() *Address {
+	return &Address{
+		StreetAddress: a.StreetAddress,
+		City:          a.City,
+		Country:       a.Country,
+	}
+}
+
+func main() {
+	john := Person{
+		Name:    "john",
+		Address: &Address{
+			StreetAddress: "123 London Rd",
+			City:          "London",
+			Country:       "UK",
+		},
+		Friends: []string{"Chris", "Matt"},
+	}
+
+	jane := john.DeepCopy()
+	jane.Name = "Jane"
+	jane.Address.StreetAddress = "312 Baker St"
+	jane.Friends = append(jane.Friends, "Angela")
+
+	/*jane := john
+	jane.Name = "Jane"
+	jane.Address = &Address{
+		StreetAddress: john.Address.StreetAddress,
+		City:          john.Address.City,
+		Country:       john.Address.Country,
+	}
+	jane.Address.StreetAddress = "321 Baker St"*/
+
+	/*jane := john
+	jane.Name = "Jane"
+	jane.Address.StreetAddress = "321 Baker St"*/
+
+	fmt.Println(john, john.Address)
+	fmt.Println(jane, jane.Address)
+}
